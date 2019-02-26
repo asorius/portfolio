c6048e320c3b35c8cbf10a3c4e492d4a08f29954
asorius <18assdd@gmail.com>
2019-02-26 19:09:15 +0000

    side navbar implementation/ width bugs on mobile view

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 (function($) {
   $(document).ready(function() {
+    const initialWidth = window.innerWidth;
+    let divider;
     $(window).scroll(function() {
-      if ($(this).scrollTop() > 600) {
+      initialWidth <= 765 ? (divider = 1.2) : (divider = 2);
+      if ($(this).scrollTop() > $(window).height() / divider) {
         $('.sticky-nav').fadeIn(500);
       } else {
         $('.sticky-nav').fadeOut(500);
