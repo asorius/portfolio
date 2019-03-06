80d2b8468198624dfa7b1f7f1ebd315e84d16328
asorius <18assdd@gmail.com>
2019-03-06 17:04:52 +0000

    skills section animations/ mobile navbar fix

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ function reduce(func, wait = 10, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+let checkPulse = e => {
+  const item = document.querySelector('.skills_container');
+  const slideInAt = window.scrollY + window.innerHeight - item.offsetHeight / 2,
+    itemBottom = item.offsetTop + item.offsetHeight,
+    isHalfShown = slideInAt > item.offsetTop,
+    isNotScrolledPast = window.scrollY < itemBottom - 100;
+
+  if (isHalfShown && isNotScrolledPast) {
+    document.querySelector('.front').classList.add('activeSlide2');
+    document.querySelector('.back').classList.add('activeSlide2');
+  } else {
+    document.querySelector('.front').classList.remove('activeSlide2');
+    document.querySelector('.back').classList.remove('activeSlide2');
+  }
+};
 
 const slideItems = document.querySelectorAll('.projects_container');
 let checkSlide = e => {
@@ -22,7 +37,7 @@ let checkSlide = e => {
         window.scrollY + window.innerHeight - item.offsetHeight / 2,
       itemBottom = item.offsetTop + item.offsetHeight,
       isHalfShown = slideInAt > item.offsetTop,
-      isNotScrolledPast = window.scrollY < itemBottom - 15;
+      isNotScrolledPast = window.scrollY < itemBottom - 100;
 
     if (isHalfShown && isNotScrolledPast) {
       document.querySelector('.dc').classList.add('activeSlide');
@@ -34,6 +49,7 @@ let checkSlide = e => {
   });
 };
 window.addEventListener('scroll', reduce(checkSlide));
+window.addEventListener('scroll', reduce(checkPulse));
 
 (function($) {
   $(document).ready(function() {
